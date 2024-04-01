30300411254e1033a239e143c5bd783dcaa220e7
hdjabi <hdjabi@gmail.com>
2024-04-01 23:23:31 +0100

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,11 @@
+#Create a variable that stores the user's name input
+#Print name
+#Create variable that stores the user's age input
+#Print age
+#Print "Git is Awesome!"
+
+name= input("Enter your name")
+print(name)
+age= int(input("Enter age"))
+print(age)
+print("Git is Awesome!")
